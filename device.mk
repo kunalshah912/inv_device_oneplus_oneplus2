23e4e4fe6819215e780fd0b27ca668ffb820f6ad
LuK1337 <priv.luk@gmail.com>
2017-09-19 09:50:10 +0530

    op2: Add basic 2.0 -> 2.1 fingerprint HAL wrapper
     * Adapt wrapper to load fp module conditionally

--- device.mk
+++ device.mk
@@ -168,7 +168,8 @@ PRODUCT_PACKAGES += \
 
 # Fingerprint sensor
 PRODUCT_PACKAGES += \
-    android.hardware.biometrics.fingerprint@2.1-service
+    android.hardware.biometrics.fingerprint@2.0-service \
+    fingerprint.msm8994
 
 # For android_filesystem_config.h
 PRODUCT_PACKAGES += \
